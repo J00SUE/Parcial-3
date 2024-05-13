cb24d6ea4c01a4fb1cbf4edd2a53a1ab83d3f130
Dari028 <dario.pantoja@udea.edu.co>
2024-05-13 15:33:16 -0500

    reestructuracion de la clase y prueba

--- Parcial 3.py
+++ Parcial 3.py
@@ -7,53 +7,56 @@ import dicom2nifti
 #from nilearn import plotting
      
 class Dicom():
+    def __init__(self) -> None:
+        self.dicomlist=[]
     def Imagen(self, path):
-        Imagen_list = []
+
         for archivo in os.listdir(path):
             if archivo.endswith('.dcm'):
-                Imagen_list.append(pydicom.dcmread(os.path.join(path, archivo)))
-        Imagen_list.sort(key = lambda x: int(x.ImagePositionPatient[2]))
-        return Imagen_list
+                self.dicomlist.append(pydicom.dcmread(os.path.join(path, archivo)))
+                self.dicomlist.sort(key = lambda x: int(x.ImagePositionPatient[2]))
+        return self.dicomlist
 
-    def Nombre(self, Lista,i):
-        Nombre = Lista[i].PatientName
+    def Nombre(self,i):
+        Nombre = self.dicomlist[i].PatientName
         return Nombre
 
-    def Age(self, Lista,i):
-        Edad = Lista[i].PatientAge
+    def Age(self,i):
+        Edad = self.dicomlist[i].PatientAge
         return Edad
 
-    def Id(self, Lista,i):
-        Id = Lista[i].PatientID
+    def Id(self,i):
+        Id = self.dicomlist[i].PatientID
         return Id
     
-    def Sex(self, Lista,i):
-        Sex = Lista[i].PatientSex
+    def Sex(self,i):
+        Sex = self.dicomlist[i].PatientSex
         return Sex
 
-    def Estudio(self, Lista,i):
-        Estudio = Lista[i].StudyDescription
+    def Estudio(self,i):
+        Estudio = self.dicomlist[i].StudyDescription
         return Estudio
-    def retornar_imagen(self,lista,i):
-        img = lista[i]
+    def retornar_imagen(self,i):
+        z= self.dicomlist[i]
+        img=z.pixel_array
         return img
 
     
 D = Dicom()
 ruta = r"C:\Users\dario\Parcial-3\Sarcoma\img1"
 A = D.Imagen(ruta)
-N = D.Nombre(A,0)
+N = D.Nombre(0)
 print('Nombre: {}' .format(N))
-E = D.Age(A,0)
+E = D.Age(0)
 print('Edad: {}' .format(E))
-p = D.Estudio(A,0)
+p = D.Estudio(0)
 print('Estudio: {}' .format(p))
-k = D.Sex(A,0)
+k = D.Sex(0)
 print('Genero: {}' .format(k))
-z=A[0]
-img=z.pixel_array
-plt.imshow(img)
-plt.axis('off')
-plt.show()
+for i in range (len(A)):
+    img=D.retornar_imagen(i)
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
 
 
